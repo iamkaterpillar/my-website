b9170f54b7da6e28b96b506f6f2cf3e5a5cd0299
iamkaterpillar <iamkaterpillar@users.noreply.github.com>
2025-05-30 20:06:42 +0000

    deploy: acbc382324e7de74ef44a43ac685d73f9efeaf14

---
+++ src/js/utils/sitemap-generator.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const path = require('path');
+
+// Read posts.json to get all blog posts
+const posts = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../public/data/posts.json'), 'utf8'));
+
+// Base URL of the website
+const baseUrl = 'https://iamkaterpillar.com';
+
+// Static pages
+const staticPages = [
+  '',  // homepage
+  'about',
+  'blog',
+  'bebop'
+];
+
+// Generate XML
+let xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+`;
+
+// Add static pages
+staticPages.forEach(page => {
+  const url = page ? `${baseUrl}/${page}` : baseUrl;
+  xml += `  <url>
+    <loc>${url}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>${page ? '0.8' : '1.0'}</priority>
+  </url>
+`;
+});
+
+// Add blog posts
+posts.forEach(post => {
+  xml += `  <url>
+    <loc>${baseUrl}/${post.slug}</loc>
+    <lastmod>${post.date}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.6</priority>
+  </url>
+`;
+});
+
+xml += '</urlset>';
+
+// Write sitemap.xml to the public directory
+fs.writeFileSync(path.join(__dirname, '../../../public/sitemap.xml'), xml);
+console.log('Sitemap generated successfully!'); 
